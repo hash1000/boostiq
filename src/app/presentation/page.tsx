2cde89b45bc67e609cfd1b99e67ae44f66c62ca8
hash1000 <hashirmughal1000@gmail.com>
2025-03-25 03:49:18 +0500

    issues fix

--- src/app/presentation/page.tsx
+++ src/app/presentation/page.tsx
@@ -32,11 +32,15 @@ export default function Presentation() {
               Recent Work
             </h4>
             <div className=" h-[90%] overflow-auto">
-              {[1, 2, 3, 4].map(() => (
-                <div className="flex flex-col gap-1 p-2.5">
+              {[1, 2, 3, 4].map((_, index) => (
+                <Link
+                  href="/assign-task"
+                  key={index}
+                  className="cursor-pointer flex flex-col gap-1 p-2.5"
+                >
                   <div className="bg-[#fff] border-1 border-black  h-56"></div>
                   <p className="text-center text-2xl font-medium">Chemistry</p>
-                </div>
+                </Link>
               ))}
             </div>
 
@@ -56,28 +60,40 @@ export default function Presentation() {
             >
               {/* Icons Section */}
               <div className="absolute right-5 top-5 bg-[#a0e4ff] flex flex-wrap md:flex-nowrap justify-center items-center gap-5 p-4 md:px-18 md:py-3">
-                <GiSandsOfTime className="text-lg md:text-2xl xl:text-3xl" />
-                
-                <Link href="/sleep-log" className="cursor-pointer">
-                <FaHeadSideVirus className="text-lg md:text-2xl xl:text-3xl" />
+                <Link href="/timmer" className="cursor-pointer">
+                  <GiSandsOfTime className="text-lg md:text-2xl xl:text-3xl" />
                 </Link>
+
+                <Link href="/stress-calculator" className="cursor-pointer">
+                  <FaHeadSideVirus className="text-lg md:text-2xl xl:text-3xl" />
+                </Link>
+
                 <Link href="/assign-task" className="cursor-pointer">
-                <FaListCheck className="text-lg md:text-2xl xl:text-3xl" />
+                  <FaListCheck className="text-lg md:text-2xl xl:text-3xl" />
                 </Link>
-                <BsPeopleFill className="text-lg md:text-2xl xl:text-3xl" />
-                <RiZzzFill className="text-lg md:text-2xl xl:text-3xl" />
-                <GrDocumentText className="text-lg md:text-2xl xl:text-3xl" />
+
+                <Link href="/planner" className="cursor-pointer">
+                  <BsPeopleFill className="text-lg md:text-2xl xl:text-3xl" />
+                </Link>
+
+                <Link href="/sleep-log" className="cursor-pointer">
+                  <RiZzzFill className="text-lg md:text-2xl xl:text-3xl" />
+                </Link>
+
+                <Link href="/notes" className="cursor-pointer">
+                  <GrDocumentText className="text-lg md:text-2xl xl:text-3xl" />
+                </Link>
               </div>
 
               {/* New Page Section */}
               <div className="w-[150px] md:w-[183px] flex flex-col gap-2.5 absolute right-10 bottom-10">
-              <p className="text-center text-lg md:text-2xl font-medium cursor-pointer hover:underline">
-                    New Page
-                  </p>
+                <p className="text-center text-lg md:text-2xl font-medium cursor-pointer hover:underline">
+                  New Page
+                </p>
                 <Link href="/doc-editor" passHref>
-                <div className="bg-[#A0E4FF] h-[180px] md:h-[220px] border-2 border-black flex justify-center items-center w-full cursor-pointer">
-                  <FaPlus className="Icon" />
-                </div>
+                  <div className="bg-[#A0E4FF] h-[180px] md:h-[220px] border-2 border-black flex justify-center items-center w-full cursor-pointer">
+                    <FaPlus className="Icon" />
+                  </div>
                 </Link>
               </div>
 
