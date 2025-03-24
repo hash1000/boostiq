4731024a8c725db840afbecc3251bcfe53474183
hash1000 <hashirmughal1000@gmail.com>
2025-03-24 14:06:19 +0500

    make traangle

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -29,13 +29,63 @@ export default function page() {
                   <br />
                 </>
               ))}
+              <div className="flex">
+
+              <div className="relative w-80 h-80 flex items-center justify-center">
+              {/* Center Circle */}
+        
+              {/* Petals (3 Triangles) */}
+              {[...Array(3)].map((_, i) => (
+                <div
+                  key={i}
+                  className="absolute w-0 h-0 border-l-[40px] border-l-transparent border-r-[40px] border-r-transparent border-b-[80px] border-b-white"
+                  style={{
+                    transform: `rotate(${i * 120}deg) translateY(40px)`,
+                  }}
+                ></div>
+              ))}
+            </div>
+
+            <div className="relative w-80 h-80 flex items-center justify-center">
+              {/* Center Circle */}
+        
+              {/* Petals (3 Triangles) */}
+              {[...Array(3)].map((_, i) => (
+                <div
+                  key={i}
+                  className="absolute w-0 h-0 border-l-[40px] border-l-transparent border-r-[40px] border-r-transparent border-b-[80px] border-b-white"
+                  style={{
+                    transform: `rotate(${i * 120}deg) translateY(40px)`,
+                  }}
+                ></div>
+              ))}
+            </div>
+              </div>
+
             </div>
 
             <div className=" w-2/3 h-full flex flex-col gap-5">
-              <div className="h-1/3 bg-[#A0E4FF] md:p-5 3xl:p-9">
+              <div className="h-1/3 bg-[#A0E4FF] md:p-5 3xl:p-9 flex flex-col">
                 <h2 className="md:text-2xl 3xl:text-4xl font-bold">
                   How did you feel today on a scale of 1-10?
                 </h2>
+                <div className="flex-1 flex justify-center items-center">
+  <div className="relative w-5/6 mx-auto">
+    {/* Main bar */}
+    <div className="bg-white h-3 w-full relative flex">
+      {/* 10 Equal Divisions */}
+      {Array.from({ length: 10 }).map((_, index) => (
+        <div key={index} className="flex-1 h-full relative">
+          {/* Divider Lines (except the last one) */}
+          <label className="absolute right-0 -top-[275%]">{index+1}</label>
+          {index < 10 && (
+            <div className="absolute right-0 -top-[100%] h-10 w-3 bg-white"></div>
+          )}
+        </div>
+      ))}
+    </div>
+  </div>
+</div>
               </div>
               <div className="h-2/3 bg-[#A0E4FF] md:p-5 3xl:p-9">
                 <h2 className="md:text-2xl 3xl:text-4xl font-bold">
